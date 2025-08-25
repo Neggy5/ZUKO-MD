63f4d44c57292b56bf5200713235907ea80bb7b2
ZUKO <132104247+Neggy5@users.noreply.github.com>
2025-08-25 01:29:49 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: '𝐙𝐔𝐊𝐎-𝐌𝐃',
+  author: '‎',
+  botName: "𝐙𝐔𝐊𝐎-𝐌𝐃",
+  botOwner: 'ZUKO', // Your name
+  ownerNumber: '2349073770402', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  aliveAudioUrl: 'https://files.catbox.moe/wwm21d.mp3',
+};
+
+module.exports = settings;
+
+
+
